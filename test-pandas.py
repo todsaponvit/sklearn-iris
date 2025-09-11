da254b5a122141eeea6cae1ffeb7aa0fc31e5a76
Todsapon Vittayasakoonchai <65076408+todsaponvit@users.noreply.github.com>
2025-09-11 23:01:21 +0700

    Update test-pandas.py

--- test-pandas.py
+++ test-pandas.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
-df = pd.read_csv("iris.csv")
+df = pd.read_csv("https://raw.githubusercontent.com/prasertcbs/basic-dataset/refs/heads/master/iris.csv")
 print(df.head())
 
-print(df.variety.value_counts())
+print(df.species.value_counts())
 
 print(df.columns)
