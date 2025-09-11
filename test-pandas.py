9b5802c7e0a656800151682193c0072a3ba0f43e
Todsapon Vittayasakoonchai <65076408+todsaponvit@users.noreply.github.com>
2025-09-11 23:07:50 +0700

    Update test-pandas.py

--- test-pandas.py
+++ test-pandas.py
@@ -6,3 +6,26 @@ print(df.head())
 print(df.species.value_counts())
 
 print(df.columns)
+
+print(df.groupby("species").mean())
+
+print(df.columns)
+
+print(df.info())
+
+# Train Dataset
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+
+test_size = 0.2
+X_train, X_test, y_train, y_test = train_test_split(
+    df[["sepal_length", "sepal_width", "petal_length", "petal_width"]],
+    df.species,
+    test_size=test_size,
+    random_state=7,
+)
+
+print(X_train)
+
+
